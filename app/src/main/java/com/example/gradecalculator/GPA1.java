937412b069a85042fb5c7e796ebfa5e1ba2c1317
natnael abreham <natiabreham728@gmail.com>
2023-09-12 15:53:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gradecalculator/GPA1.java
@@ -0,0 +1,14 @@
+package com.example.gradecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GPA1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpa1);
+    }
+}
